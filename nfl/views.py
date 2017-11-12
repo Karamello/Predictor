abd46be4087db8437e950a2c06fca67439966508
Karamello <matt.riley95@hotmail.co.uk>
2017-11-12 00:07:51 +0000

    Added template to test db data

--- nfl/views.py
+++ nfl/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Team
 
 # Create your views here.
+
+def showteams(request):
+    teams = Team.objects.all()
+    return render(request, 'nfl/teams.html',{'teams':teams})
